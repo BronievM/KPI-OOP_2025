64a276a27efb0f3cf29e3a5300a14c43a8362dd8
Broniev <bronievcorp@gmail.com>
2025-04-14 23:47:54 +0300

    Update!!

--- LR4/obj/Debug/net8.0/LR4.AssemblyInfo.cs
+++ LR4/obj/Debug/net8.0/LR4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LR4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46371d60e4ec08bc079104faaa948ea381f2b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("LR4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LR4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
